ee7ce1e6c9f7bd2047a0f8e9d6155bd5773b6937
Vlad Riscutia <vladris@outlook.com>
2022-09-03 20:16:34 -0700

    Generate index and responsive text size

--- baku/index.py
+++ baku/index.py
@@ -7,17 +7,20 @@ def build_index(posts):
     template = templating.Template(
         os.path.join('templates', consts.ROOT_TEMPLATE))
 
-    html = ''
+    html = '<div id="index">'
     year = ''
     for post in posts:
         if post.year != year:
-            html += f'<h2>{post.year}</h2>'
+            html += f'<div class="year"><span>{post.year}</span></div>'
             year = post.year
 
         month = datetime.strftime(post.date, '%b')
 
-        html += f'<p>{month} {post.day} <a href={post.rel_path}>' + \
-            f'{misaka.escape_html(post.title)}</a></p>\n'
+        html += f'<div class="date"><span>{month} {post.day}</span></div>' + \
+            f'<div class="post"><span><a href="{post.rel_path}">' + \
+            f'{misaka.escape_html(post.title)}</a></span></div>\n'
+
+    html += '</div>'
 
     context = {'body': html}
 
