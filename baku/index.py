02fb531addaf54dc74ad7e65b6b2277cf12693f8
Vlad Riscutia <vladris@outlook.com>
2024-03-22 19:27:12 -0700

    Fix bug that drops posts from index

--- baku/index.py
+++ baku/index.py
@@ -8,16 +8,19 @@ def build_index(posts: List[post.Post], config: Dict[str, str]):
         os.path.join('templates', consts.ROOT_TEMPLATE))
 
     # Group posts by year
-    year, context, posts_in_year = posts[0].date.year, {'years': []}, []
+    posts_by_year, context = {}, {'years': []}
     for p in posts:
-        if p.date.year == year:
-            posts_in_year.append(p)
-        else:
-            context['years'].append({
-                'year': year,
-                'posts': posts_in_year
-            })
-            year, posts_in_year = p.date.year, [p]
+        if p.date.year not in posts_by_year:
+            posts_by_year[p.date.year] = []
+
+        posts_by_year[p.date.year].append(p)
+
+    # Generate context
+    for year in sorted(posts_by_year.keys(), reverse=True):
+        context['years'].append({
+            'year': year,
+            'posts': posts_by_year[year]
+        })
 
     with utils.open_utf8(os.path.join('.', 'html', 'index.html'), 'w+') as f:
         f.write(templating.render(template, context | config))
